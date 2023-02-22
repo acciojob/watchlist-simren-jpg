ed3eb2fc957e6744dee20ea229c901822bb8b8e7
Shalini <shalinipaliwalg@gmail.com>
2023-02-22 20:23:35 +0530

    watchlist

--- src/main/java/com/driver/Movie.java
+++ src/main/java/com/driver/Movie.java
@@ -2,40 +2,30 @@ package com.driver;
 
 public class Movie {
     private String name;
-    private int duration;
+    private int durationInMinutes;
     private double imdbRating;
-
-    public Movie() {
-    }
-
-    public Movie(String name, int duration, double imdbRating) {
+    public Movie() {}
+    public Movie(String name, int durationInMinutes, double imdbRating) {
         this.name = name;
-        this.duration = duration;
+        this.durationInMinutes = durationInMinutes;
         this.imdbRating = imdbRating;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
-    public int getDuration() {
-        return duration;
+    public int getDurationInMinutes() {
+        return durationInMinutes;
     }
-
-    public void setDuration(int duration) {
-        this.duration = duration;
+    public void setDurationInMinutes(int durationInMinutes) {
+        this.durationInMinutes = durationInMinutes;
     }
-
     public double getImdbRating() {
         return imdbRating;
     }
-
     public void setImdbRating(double imdbRating) {
         this.imdbRating = imdbRating;
     }
 }
-
